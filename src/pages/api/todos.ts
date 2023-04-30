3681352f5be9dc3fd2b939e25e10f244b602db1b
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-04-30 14:50:18 +0900

    todo create

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import client from "@/lib/dynamodb";
 import { ErrorResponse } from "@/type/err";
+import { v4 as uuid } from "uuid";
 import { Todo } from "@/type/todo";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -21,6 +22,31 @@ export default async function handler(
       } catch (err) {
         res.status(500).json({ err: "Something went wrong" });
       }
+      break;
+
+    case "POST":
+      try {
+        if (!process.env.DYNAMODB_TABLE_NAME) {
+          throw new Error("DYNAMODB_TABLE_NAME is not defined");
+        }
+        const { title } = req.body;
+        const newTodo: Todo = {
+          todoId: uuid(),
+          title,
+        };
+        await client
+          .put({
+            TableName: process.env.DYNAMODB_TABLE_NAME,
+            Item: newTodo,
+          })
+          .promise();
+        res.status(200).json(newTodo);
+      } catch (err) {
+        res.status(500).json({ err: "Something went wrong" });
+      }
+      break;
+    default:
+      res.status(405).json({ err: "Method not supported" });
       break;
   }
 }
